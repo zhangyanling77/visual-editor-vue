d14ec726086423a3358df4b4647e0c5c25432a7d
zhangyanling <18783226594@163.com>
2021-01-23 18:20:55 +0800

    feat:主页面结构

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <h1>这是页面内容</h1>
+    <visual-editor />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { VisualEditor } from '@/packages/visual-editor';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    VisualEditor,
   }
 });
 </script>
 
 <style lang="scss">
-#app {
+html, body {
+  margin: 0;
+  padding: 0;
+}
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/packages/visual-editor.tsx
@@ -0,0 +1,26 @@
+import { defineComponent } from 'vue';
+import './visual-editor.scss';
+
+export const VisualEditor = defineComponent({
+  props: {},
+  setup(props) {
+    return () => (
+      <div class="visual-editor">
+        <div class="visual-editor-menu">
+          visual-editor-menu
+        </div>
+        <div class="visual-editor-head">
+          visual-editor-head
+        </div>
+        <div class="visual-editor-operator">
+          visual-editor-operator
+        </div>
+        <div class="visual-editor-body">
+          <div class="visual-editor-content">
+            visual-editor-content
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
