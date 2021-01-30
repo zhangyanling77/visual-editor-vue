8b3f0734de9f3cff2526b22da517f546393e781d
zhangyanling <18783226594@163.com>
2021-01-30 10:14:46 +0800

    feat: 更改主题色

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import './element-variables.scss'
 import App from './App.vue';
 
 const app = createApp(App);
